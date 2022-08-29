474b2d604190b2de9092dce7cf9509d93446f60e
ignacioborraz <ignacioborraz@hotmail.com>
2022-08-28 23:56:36 -0300

    back

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
-let apiUrl = 'http://localhost:8000/'
+//let apiUrl = 'http://localhost:8000/'
 
-//let apiUrl = 'https://back-marble.herokuapp.com/'
+let apiUrl = 'https://back-marble.herokuapp.com/'
 
 export default apiUrl
