5023bb6cba584ef0f93169628633199f68d0a900
ignacioborraz <ignacioborraz@hotmail.com>
2022-12-28 16:59:29 -0300

    api

--- src/url.js
+++ src/url.js
@@ -1,7 +1,9 @@
 let apiUrl = 'http://localhost:8000/'
+console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV==='production') {
     apiUrl = process.env.REACT_APP_URL
 }
+console.log(apiUrl)
 
 export default apiUrl
