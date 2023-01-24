777adcf1f554312a314091a04f16b5008cdd0a22
ignacioborraz <ignacioborraz@hotmail.com>
2023-01-23 23:55:51 -0300

    updating

--- src/index.js
+++ src/index.js
@@ -5,22 +5,14 @@ import './styles/index.css'
 
 import App from './App'
 
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import {Provider} from 'react-redux'
-import {configureStore as createStore} from '@reduxjs/toolkit'
-import mainReducer from './redux/reducers/mainReducer'
-
-const reduxStore = createStore({reducer: mainReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ //error de mutacion
-        immutableCheck: false,
-        serializableCheck: false,
-   })})
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-    <Provider store={reduxStore}>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
