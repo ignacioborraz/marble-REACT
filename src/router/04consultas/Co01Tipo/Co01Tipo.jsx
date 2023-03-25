555026998e79e8d6788ac86ca37a24aa15cc7bc8
ignacioborraz <ignacioborraz@hotmail.com>
2023-03-24 23:42:47 -0300

    fix

--- src/router/04consultas/Co01Tipo/Co01Tipo.jsx
+++ src/router/04consultas/Co01Tipo/Co01Tipo.jsx
@@ -5,22 +5,29 @@ import './co01Tipo.css'
 import SquareIcon from '@mui/icons-material/Square'
 import HexagonIcon from '@mui/icons-material/Hexagon'
 import TokenIcon from '@mui/icons-material/Token'
+import InventoryIcon from '@mui/icons-material/Inventory'
 
 export default function Co01Tipo() {
     return (
-        <div className='type-container' >
-            <div className='type-box' >
-                <LinkRouter className='type-link' to={'/stocks/internal'}>
-                    <HexagonIcon className='type-icon' />
+        <div className='types-container'>
+            <div className='types-box'>
+                <LinkRouter className='types-link' to={'/stocks/internal'}>
+                    <HexagonIcon className='types-icon' />
                     <h2>INTERNOS</h2>
                 </LinkRouter>
-                <LinkRouter className='type-link' to={'/stocks/note'}>
-                    <TokenIcon className='type-icon' />
+                <LinkRouter className='types-link' to={'/stocks/note'}>
+                    <TokenIcon className='types-icon' />
                     <h2>PEDIDOS</h2>
                 </LinkRouter>
-                <LinkRouter className='type-link' to={'/stocks/done'}>
-                    <SquareIcon className='type-icon' />
+            </div>
+            <div className='types-box'>
+                <LinkRouter className='types-link' to={'/stocks/done'}>
+                    <SquareIcon className='types-icon' />
                     <h2>ENTREGADOS</h2>
+                </LinkRouter>
+                <LinkRouter className='types-link' to={'/stocks/total'}>
+                    <InventoryIcon className='types-icon' />
+                    <h2>STOCK</h2>
                 </LinkRouter>
             </div>
         </div>        
