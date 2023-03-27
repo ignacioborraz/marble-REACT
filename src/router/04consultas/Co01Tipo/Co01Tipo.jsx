bc44b9813bfd16168d03084e3f998e3206453ace
ignacioborraz <ignacioborraz@hotmail.com>
2023-03-27 13:06:06 -0300

    fix

--- src/router/04consultas/Co01Tipo/Co01Tipo.jsx
+++ src/router/04consultas/Co01Tipo/Co01Tipo.jsx
@@ -25,7 +25,7 @@ export default function Co01Tipo() {
                     <SquareIcon className='types-icon' />
                     <h2>ENTREGADOS</h2>
                 </LinkRouter>
-                <LinkRouter className='types-link' to={'/all/ksinks'}>
+                <LinkRouter className='types-link' to={'/all/ksink'}>
                     <InventoryIcon className='types-icon' />
                     <h2>STOCK</h2>
                 </LinkRouter>
